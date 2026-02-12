e0336926ef64573ba368bf40bdb17824eba4dd48
Igz <st25.130@02-103B-011503>
2026-02-12 17:14:16 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    double start = 0.0;
+    double end = 4.0;
+    int points = 6;
+    double step = end / points;
+
+    std::cout << "Calculating sin(x) in 6 points on interval (0, 4]:" << std::endl;
+    for (int i = 1; i <= points; i++) {
+        double x = i * step;
+        double y = sin(x);
+        std::cout << "x = " << x << ", sin(x) = " << y << std::endl;
+    }
+
+    return 0;
+}
